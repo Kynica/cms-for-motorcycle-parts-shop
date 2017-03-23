b18db25258837716b7382a62d8a8fc82be91ed88
Kynica <dmitry.kozyuk@gmail.com>
2017-03-23 16:52:14 +0200

    Add Profile table model

---
+++ common/models/Profile.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "profile".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property string  $name
+ * @property string  $surname
+ * @property string  $patronymic
+ *
+ * @property User $user
+ */
+class Profile extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%profile}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'name', 'surname'], 'required'],
+            [['user_id'], 'integer'],
+            [['name', 'surname', 'patronymic'], 'string', 'max' => 45],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id'         => 'ID',
+            'user_id'    => 'User ID',
+            'name'       => 'Name',
+            'surname'    => 'Surname',
+            'patronymic' => 'Patronymic',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
