904c33897ce2bf9c9e54111a16a3ce935e17fadf
Kynica <dmitry.kozyuk@gmail.com>
2017-03-27 23:29:41 +0300

    Create model for Category table.

---
+++ common/models/Category.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property string $name
+ */
+class Category extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%category}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id'   => Yii::t('category', 'ID'),
+            'name' => Yii::t('category', 'Name'),
+        ];
+    }
+}
