c993c6e2d59357c068f3abbfd8d54ec3d575d316
Kynica <dmitry.kozyuk@gmail.com>
2017-03-28 10:32:48 +0300

    View list of parent categories.

--- backend/views/category/_form.php
+++ backend/views/category/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
+use common\models\Category;
 
 /**
  * @var $this yii\web\View
@@ -14,7 +16,25 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    <div class="row">
+        <div class="col-md-4">
+            <?= $form->field($model, 'parent_id')->widget(
+                Select2::className(),
+                [
+                    'data' => Category::getTreeForSelect($model->id),
+                    'options' => [
+                        'placeholder' => 'Select a category ...',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]
+            ) ?>
+        </div>
+        <div class="col-md-8">
+            <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
