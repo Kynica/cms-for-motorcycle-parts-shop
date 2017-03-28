2acf68a0d90fb0dbeba38ddbefd69539a675eefa
Kynica <dmitry.kozyuk@gmail.com>
2017-03-28 21:11:42 +0300

    Add methods to manipulate category relation hierarchy.

--- common/models/CategoryClosure.php
+++ common/models/CategoryClosure.php
@@ -48,4 +48,66 @@ class CategoryClosure extends ActiveRecord
             'depth' => 'Depth',
         ];
     }
+
+    public static function updateFor(Category $category)
+    {
+        static::deleteFor($category);
+        static::createFor($category);
+        static::updateForChildren($category);
+
+        return;
+    }
+
+    protected static function createFor(Category $category)
+    {
+        if (! empty($category->parent_id)) {
+            /** @var Category $parentCategory */
+            $parentCategory = Category::find()
+                ->with(['parents'])
+                ->where(['id' => $category->parent_id])
+                ->one();
+
+            $depth = 0;
+
+            foreach ($parentCategory->parents as $parent) {
+                $new = new CategoryClosure([
+                    'ancestor'   => $parent->id,
+                    'descendant' => $category->id,
+                    'depth'      => $depth
+                ]);
+
+                $new->save();
+
+                $depth++;
+            }
+
+            $new = new CategoryClosure([
+                'ancestor'   => $parentCategory->id,
+                'descendant' => $category->id,
+                'depth'      => $depth
+            ]);
+
+            $new->save();
+        }
+
+        return;
+    }
+
+    protected static function deleteFor(Category $category)
+    {
+        Yii::$app->db->createCommand()
+            ->delete(CategoryClosure::tableName(), ['descendant' => $category->id])
+            ->execute();
+    }
+
+    protected static function updateForChildren(Category $category)
+    {
+        foreach ($category->children as $child) {
+            static::deleteFor($child);
+            static::createFor($child);
+
+            if (count($child->children))
+                static::updateForChildren($child);
+        }
+    }
 }
