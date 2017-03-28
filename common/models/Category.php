1a5ac364bcb14fd125dce6d4adcffff856baf32b
Kynica <dmitry.kozyuk@gmail.com>
2017-03-28 10:32:25 +0300

    Add methods which allow to get categories as tree for drop down list.

--- common/models/Category.php
+++ common/models/Category.php
@@ -4,6 +4,7 @@ namespace common\models;
 
 use Yii;
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "category".
@@ -53,5 +54,43 @@ class Category extends ActiveRecord
     public function getParent()
     {
         return $this->hasOne(Category::className(), ['id' => 'parent_id']);
+    }
+
+    public static function getTreeForSelect($ignoreCategoryId = null)
+    {
+        /** @var static[] $categories */
+        $categories = static::find()->indexBy('id')->all();
+
+        foreach ($categories as $category) {
+            if ($category->parent_id === null) {
+                $category->parent_id = 0;
+            }
+        }
+
+        if (! empty($ignoreCategoryId)) {
+            unset($categories[ $ignoreCategoryId ]);
+        }
+
+        $categories = ArrayHelper::map($categories, 'id', 'name', 'parent_id');
+
+        return static::createTree($categories, $categories[0]);
+    }
+
+    protected static function createTree($allCategories, $currentCategories, &$tree = [], $depth = 0)
+    {
+        $beforeName = '';
+
+        for ($i = 0; $i < $depth; $i++) {
+            $beforeName .= '-';
+        }
+
+        foreach ($currentCategories as $id => $name) {
+            $tree[ $id ] = $beforeName . $name;
+            if (isset($allCategories[ $id ])) {
+                static::createTree($allCategories, $allCategories[ $id ], $tree, $depth += 1);
+            }
+        }
+
+        return $tree;
     }
 }
