a0150568d244048c8c9e8cd1b0f6700451d5c4e3
Kynica <dmitry.kozyuk@gmail.com>
2017-03-29 11:24:51 +0300

    Fix: add dropIndex to migration down.

--- console/migrations/m170327_124354_add_currency_id_column_to_product_table.php
+++ console/migrations/m170327_124354_add_currency_id_column_to_product_table.php
@@ -15,6 +15,7 @@ class m170327_124354_add_currency_id_column_to_product_table extends Migration
         $this->addColumn('{{%product}}', 'currency_id', $this->integer());
 
         $this->createIndex('idx-product-currency_id', '{{%product}}', 'currency_id');
+
         $this->addForeignKey(
             'fk-product-currency_id',
             '{{%product}}',
@@ -32,6 +33,8 @@ class m170327_124354_add_currency_id_column_to_product_table extends Migration
     public function safeDown()
     {
         $this->dropForeignKey('fk-product-currency_id', '{{%product}}');
+
+        $this->dropIndex('idx-product-currency_id', '{{%product}}');
 
         $this->dropColumn('{{%product}}', 'currency_id');
     }
