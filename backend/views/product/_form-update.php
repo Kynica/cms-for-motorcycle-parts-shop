c7b28ca07d1059a4d8fe2ebd3a7e71f9deaf8b59
Kynica <dmitry.kozyuk@gmail.com>
2017-03-29 11:39:49 +0300

    Add opportunity to add or remove product from category.

--- backend/views/product/_form-update.php
+++ backend/views/product/_form-update.php
@@ -3,12 +3,16 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
+use kartik\select2\Select2;
 use common\models\Product;
 use common\models\Currency;
+use common\models\Category;
 
-/* @var $this yii\web\View */
-/* @var $model common\models\Product */
-/* @var $form yii\widgets\ActiveForm */
+/**
+ * @var $this yii\web\View
+ * @var $model common\models\Product
+ * @var $form yii\widgets\ActiveForm
+ */
 ?>
 
 <div class="product-form">
@@ -19,16 +23,30 @@ use common\models\Currency;
         <div class="col-md-3">
             <?= $form->field($model, 'sku')->textInput(['maxlength' => true]) ?>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-2">
+            <?= $form->field($model, 'stock')->dropDownList(Product::getStockVariation()) ?>
+        </div>
+        <div class="col-md-7">
             <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
         </div>
     </div>
 
     <div class="row">
         <div class="col-md-3">
-            <?= $form->field($model, 'stock')->dropDownList(Product::getStockVariation()) ?>
+            <?= $form->field($model, 'category_id')->widget(
+                Select2::className(),
+                [
+                    'data' => Category::getTreeForSelect(),
+                    'options' => [
+                        'placeholder' => 'Select a category ...',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]
+            ) ?>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-2">
             <?= $form->field($model, 'currency_id')->dropDownList(ArrayHelper::map(Currency::find()->all(), 'id', 'name')) ?>
         </div>
         <div class="col-md-2">
