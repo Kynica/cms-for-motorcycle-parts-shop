1c8f12b20cc138ad4163694f98edfe0f56011ad0
Kynica <dmitry.kozyuk@gmail.com>
2017-03-29 11:23:46 +0300

    Create migration for adding category_id column to product table.

---
+++ console/migrations/m170329_081617_add_category_id_column_to_product_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding category_id to table `product`.
+ */
+class m170329_081617_add_category_id_column_to_product_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%product}}', 'category_id', $this->integer());
+
+        $this->createIndex('idx-product-category_id', '{{%product}}', 'category_id');
+
+        $this->addForeignKey(
+            'fk-product-category_id',
+            '{{%product}}',
+            'category_id',
+            '{{%category}}',
+            'id',
+            'SET NULL',
+            'SET NULL'
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-product-category_id', '{{%product}}');
+
+        $this->dropIndex('idx-product-category_id', '{{%product}}');
+
+        $this->dropColumn('{{%product}}', 'category_id');
+    }
+}
