9e5a733b864ee69b44246f6e5ec0956acec1e24c
Kynica <dmitry.kozyuk@gmail.com>
2017-03-30 11:58:24 +0300

    Add methods for work with upload images.

--- common/models/ProductImage.php
+++ common/models/ProductImage.php
@@ -3,7 +3,10 @@
 namespace common\models;
 
 use Yii;
+use yii\helpers\Url;
+use yii\base\Exception;
 use yii\db\ActiveRecord;
+use yii\db\Expression;
 
 /**
  * This is the model class for table "product_image".
@@ -17,6 +20,7 @@ use yii\db\ActiveRecord;
  */
 class ProductImage extends ActiveRecord
 {
+    protected static $images = [];
     /**
      * @inheritdoc
      */
@@ -57,5 +61,79 @@ class ProductImage extends ActiveRecord
     public function getProduct()
     {
         return $this->hasOne(Product::className(), ['id' => 'product_id']);
+    }
+
+    public static function getStorageFolder(Product $product)
+    {
+        return Yii::$app->params['uploadDir'] . '/' . 'product' . '/' . $product->id;
+    }
+
+    public static function uploadFor(Product $product)
+    {
+        $imageNumber = static::find()->where(['product_id' => $product->id])->count();
+        $newImages   = File::uploadImages(static::getStorageFolder($product), $product->name);
+
+        foreach ($newImages as $name) {
+            $imageNumber += 1;
+            $new = new static([
+                'product_id' => $product->id,
+                'path'       => $name,
+                'sort'       => $imageNumber
+            ]);
+
+            if (! $new->save())
+                throw new Exception('Can\'t save new image to database');
+        }
+
+        return;
+    }
+
+    public static function deleteOne($id)
+    {
+        $image = static::findOne($id);
+        if ($image->delete()) {
+            File::delete($image->path);
+            Yii::$app->db->createCommand()
+                ->update(
+                    static::tableName(),
+                    ['sort' => new Expression('sort - 1')],
+                    'product_id = ' . $image->product_id . ' AND sort > ' . $image->sort
+                )->execute();
+        }
+        return;
+    }
+
+    public static function loadImagesFor(Product $product)
+    {
+        if (! array_key_exists($product->id, static::$images)) {
+            static::$images[ $product->id ] = static::find()
+                ->where(['product_id' => $product->id])
+                ->orderBy(['sort' => SORT_ASC])
+                ->indexBy('sort')
+                ->all();
+        }
+    }
+
+    public static function getImages(Product $product)
+    {
+        static::loadImagesFor($product);
+
+        $images = [];
+        foreach (static::$images[ $product->id ] as $image) {
+            $images[] = 'http://cms-for-motorcycle-parts-shop.loc' . $image->path;
+        }
+        return $images;
+    }
+
+    public static function getImagesData(Product $product)
+    {
+        $images = [];
+        foreach (static::$images[ $product->id ] as $image) {
+            /** @var $image ProductImage */
+            $images[] = [
+                'url' => Url::to(['/product/image-delete', 'imageId' => $image->id])
+            ];
+        }
+        return $images;
     }
 }
