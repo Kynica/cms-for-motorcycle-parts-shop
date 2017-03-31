a4dc28856c9990c51a69c71a5564b47bf3e5fc76
Kynica <dmitry.kozyuk@gmail.com>
2017-03-31 11:04:33 +0300

    Add Supplier model.

---
+++ common/models/Supplier.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+use yii\behaviors\SluggableBehavior;
+
+/**
+ * This is the model class for table "supplier".
+ *
+ * @property integer $id
+ * @property string  $name
+ * @property string  $code
+ */
+class Supplier extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%supplier}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'code'], 'required'],
+            [['name', 'code'], 'string', 'max' => 45],
+        ];
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => SluggableBehavior::className(),
+                'slugAttribute' => 'code'
+            ]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id'   => Yii::t('supplier', 'ID'),
+            'name' => Yii::t('supplier', 'Name'),
+            'code' => Yii::t('supplier', 'Code'),
+        ];
+    }
+}
