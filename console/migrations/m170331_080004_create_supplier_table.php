3f935c40995e1ed5678b7e9cd9078b78879a34c8
Kynica <dmitry.kozyuk@gmail.com>
2017-03-31 11:01:50 +0300

    Add migration for supplier table.

---
+++ console/migrations/m170331_080004_create_supplier_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `supplier`.
+ */
+class m170331_080004_create_supplier_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%supplier}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(45)->notNull(),
+            'code' => $this->string(45)->notNull()
+        ], $tableOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%supplier}}');
+    }
+}
