1193dd193d5d6da93839db92b4fe38b264ed440f
Kynica <dmitry.kozyuk@gmail.com>
2017-03-31 15:17:53 +0300

    Create model SupplierProduct for table supplier_product.

---
+++ common/models/SupplierProduct.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "supplier_product".
+ *
+ * @property integer $id
+ * @property integer $supplier_id
+ * @property integer $product_id
+ * @property string  $sku
+ * @property string  $link
+ *
+ * @property Product  $product
+ * @property Supplier $supplier
+ */
+class SupplierProduct extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%supplier_product}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['supplier_id', 'sku'], 'required'],
+            [['supplier_id', 'product_id'], 'integer'],
+            [['sku'], 'string', 'max' => 25],
+            [['link'], 'string', 'max' => 255],
+            [['product_id'], 'exist', 'skipOnError' => true, 'targetClass' => Product::className(), 'targetAttribute' => ['product_id' => 'id']],
+            [['supplier_id'], 'exist', 'skipOnError' => true, 'targetClass' => Supplier::className(), 'targetAttribute' => ['supplier_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id'          => 'ID',
+            'supplier_id' => 'Supplier ID',
+            'product_id'  => 'Product ID',
+            'sku'         => 'Sku',
+            'link'        => 'Link',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProduct()
+    {
+        return $this->hasOne(Product::className(), ['id' => 'product_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSupplier()
+    {
+        return $this->hasOne(Supplier::className(), ['id' => 'supplier_id']);
+    }
+}
