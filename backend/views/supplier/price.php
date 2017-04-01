56478cca211583cca38807ceae38361fa6c978c0
Kynica <dmitry.kozyuk@gmail.com>
2017-04-01 11:26:59 +0300

    Add form for configuration price parser: select product category and currency.

--- backend/views/supplier/price.php
+++ backend/views/supplier/price.php
@@ -1,7 +1,13 @@
 <?php
 
+use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
 use kartik\file\FileInput;
+use kartik\select2\Select2;
+use common\models\Category;
+use common\models\Currency;
 
 /**
  * @var $this  yii\web\View
@@ -12,24 +18,69 @@ use kartik\file\FileInput;
 <div class="price">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <?= FileInput::widget([
-                'name' => 'File[upload]',
-                'options'=>[
-                    'multiple'=>false
-                ],
-                'pluginOptions' => [
-                    'uploadAsync' => false,
-                    'initialPreview' => empty($model->getPriceUrl()) ? '' : $model->getPriceUrl(),
-                    'initialPreviewAsData'=>true,
-                    'initialCaption'=>"",
-                    'initialPreviewConfig' => $model->getPriceData(),
-                    'uploadUrl' => Url::to(['/supplier/price', 'id' => $model->id]),
-                    'uploadExtraData' => [
-                        'supplier_id'  => $model->id,
+            <div class="form-group">
+                <?= FileInput::widget([
+                    'name' => 'File[upload]',
+                    'options'=>[
+                        'multiple'=>false
                     ],
-                    'maxFileCount' => 1
-                ]
+                    'pluginOptions' => [
+                        'uploadAsync' => false,
+                        'initialPreview' => empty($model->getPriceUrl()) ? '' : $model->getPriceUrl(),
+                        'initialPreviewAsData'=>true,
+                        'initialCaption'=>"",
+                        'initialPreviewConfig' => $model->getPriceData(),
+                        'uploadUrl' => Url::to(['/supplier/price', 'id' => $model->id]),
+                        'uploadExtraData' => [
+                            'supplier_id'  => $model->id,
+                        ],
+                        'maxFileCount' => 1
+                    ]
+                ]) ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <?php $form = ActiveForm::begin([
+                'method' => 'get',
+                'action' => ['/supplier/processing-price']
             ]) ?>
+
+            <?= Html::input('hidden', 'supplierId', $model->id) ?>
+
+            <div class="row form-group">
+                <div class="col-md-3">
+                    <?= Select2::widget([
+                        'name' => 'categoryId',
+                        'data' => Category::getTreeForSelect(),
+                        'options' => [
+                            'placeholder' => 'Select a category ...',
+                        ],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ]) ?>
+                </div>
+
+                <div class="col-md-3">
+                    <?= Select2::widget([
+                        'name' => 'currencyId',
+                        'data' => ArrayHelper::map(Currency::find()->all(), 'id', 'name'),
+                        'options' => [
+                            'placeholder' => 'Select a currency ...',
+                        ],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ]) ?>
+                </div>
+            </div>
+
+            <?= Html::submitButton(Yii::t('supplier', 'Processing price'), ['class' => 'btn btn-primary']) ?>
+
+            <?php ActiveForm::end() ?>
         </div>
     </div>
 </div>
