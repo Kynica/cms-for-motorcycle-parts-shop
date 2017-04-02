a00693d31018cfa7c6628e1a88e2fb22117ddbab
Kynica <dmitry.kozyuk@gmail.com>
2017-04-02 11:38:06 +0300

    Create CategoryProductMargin model from category_product_margin table.

---
+++ common/models/CategoryProductMargin.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "category_product_margin".
+ *
+ * @property integer $id
+ * @property integer $category_id
+ * @property integer $currency_id
+ * @property string  $margin_type
+ * @property string  $margin
+ *
+ * @property Currency $currency
+ * @property Category $category
+ */
+class CategoryProductMargin extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%category_product_margin}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['category_id', 'currency_id'], 'required'],
+            [['category_id', 'currency_id'], 'integer'],
+            [['margin_type'], 'string'],
+            [['margin'], 'number'],
+            [['currency_id'], 'exist', 'skipOnError' => true, 'targetClass' => Currency::className(), 'targetAttribute' => ['currency_id' => 'id']],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::className(), 'targetAttribute' => ['category_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id'          => Yii::t('category-product-margin', 'ID'),
+            'category_id' => Yii::t('category-product-margin', 'Category ID'),
+            'currency_id' => Yii::t('category-product-margin', 'Currency ID'),
+            'margin_type' => Yii::t('category-product-margin', 'Margin Type'),
+            'margin'      => Yii::t('category-product-margin', 'Margin'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCurrency()
+    {
+        return $this->hasOne(Currency::className(), ['id' => 'currency_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
+    }
+}
