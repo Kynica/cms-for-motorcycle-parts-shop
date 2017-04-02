3e4d507853ab92f075ebb0eca60684f015c1da19
Kynica <dmitry.kozyuk@gmail.com>
2017-04-02 17:59:00 +0300

    Add view Product price.

--- backend/views/product/_form-update.php
+++ backend/views/product/_form-update.php
@@ -67,6 +67,22 @@ use common\models\ProductImage;
 
     <div class="row">
         <div class="col-md-3">
+
+        </div>
+
+        <div class="col-md-2">
+            <div class="form-group">
+                <label><?= $model->getAttributeLabel('price') ?></label>
+                <?= Html::input('text', 'price', $model->price, [
+                    'class' => 'form-control',
+                    'disabled' => true
+                ]) ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-3">
             <div class="form-group">
                 <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-primary']) ?>
             </div>
