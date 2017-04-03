49e5dd37293f3232350610b110a3dcd92887fc37
Kynica <dmitry.kozyuk@gmail.com>
2017-04-03 17:30:38 +0300

    Create Product model for frontend.

---
+++ frontend/models/Product.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace frontend\models;
+
+use common\models\Product AS P;
+use common\models\ProductImage;
+
+class Product extends P
+{
+    const PRICE_NOT_SET = '0.00';
+
+    public function isDiscountSet()
+    {
+        $discount = false;
+
+        if (static::PRICE_NOT_SET != $this->old_price && $this->old_price > $this->sell_price) {
+            $discount = true;
+        }
+
+        return $discount;
+    }
+
+    public function getDiscount()
+    {
+        return 0;
+    }
+
+    public function getOldPrice()
+    {
+        return $this->old_price;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getMainImage($width = 200, $height = 200, $quality = 100, $scope = 'global')
+    {
+        return ProductImage::getImageFromCache($this, $width = 200, $height = 200, $quality = 100, $scope = 'global');
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getUrl()
+    {
+        return $this->page->url;
+    }
+}
