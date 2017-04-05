4657918bb54adf14afe129ea311335a758d7b4b1
Kynica <dmitry.kozyuk@gmail.com>
2017-04-05 16:52:17 +0300

    Update cart migration. Add columns is_ordered, ordered_at.

--- console/migrations/m170404_133926_create_cart_table.php
+++ console/migrations/m170404_133926_create_cart_table.php
@@ -22,10 +22,13 @@ class m170404_133926_create_cart_table extends Migration
             'id'         => $this->primaryKey(),
             'key'        => $this->string(32)->notNull(),
             'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull()
+            'updated_at' => $this->integer()->notNull(),
+            'is_ordered' => "ENUM('yes', 'no') NOT NULL DEFAULT 'no'",
+            'ordered_at' => $this->integer()->defaultValue(NULL)
         ], $tableOptions);
 
-        $this->createIndex('idx-cart-key', '{{%cart}}', 'key');
+        $this->createIndex('idx-cart-key',        '{{%cart}}', 'key');
+        $this->createIndex('idx-cart-is_ordered', '{{%cart}}', 'is_ordered');
     }
 
     /**
