04642bd320552881455f4f3a99a7437ead805307
Kynica <dmitry.kozyuk@gmail.com>
2017-04-05 10:44:12 +0300

    Create CartController.

---
+++ frontend/controllers/CartController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\Exception;
+use yii\web\Controller;
+use frontend\models\Cart;
+
+class CartController extends Controller
+{
+    public function actionIndex()
+    {
+        $cartKey = Yii::$app->request->cookies->getValue('cart');
+        $cart    = Cart::getByKey($cartKey);
+
+        if (! empty($cartKey) && empty($cart)) {
+            Yii::$app->response->cookies->remove('cart');
+        }
+
+        return $this->render('index', [
+            'cart' => $cart
+        ]);
+    }
+
+    public function actionAddProduct()
+    {
+        $cartKey   = Yii::$app->request->cookies->getValue('cart');
+        $productId = (int) Yii::$app->request->post('product');
+
+        if (empty($productId)) {
+            return $this->redirect(['cart/index'])->send();
+        }
+
+        try {
+            $cart = Cart::getByKey($cartKey);
+        } catch (Exception $e) {
+            return $this->redirect(['cart/index'])->send();
+        }
+
+        try {
+            $cart->addProduct($productId);
+        } catch (Exception $e) {
+            $cart->deleteIfEmpty();
+            return $this->redirect(['cart/index'])->send();
+        }
+
+        return $this->redirect(['cart/index'])->send();
+    }
+
+    public function actionRemoveProduct()
+    {
+
+    }
+
+    public function beforeAction($action)
+    {
+        $this->enableCsrfValidation = false;
+
+        return parent::beforeAction($action);
+    }
+}
