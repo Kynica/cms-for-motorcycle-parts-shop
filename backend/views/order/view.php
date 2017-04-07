e25f9288411f075129d1e6830bb4d606e203ee0f
Kynica <dmitry.kozyuk@gmail.com>
2017-04-07 15:05:56 +0300

    View Customer info and ordered product info.

--- backend/views/order/view.php
+++ backend/views/order/view.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\DetailView;
 
 /* @var $this  yii\web\View */
@@ -14,11 +15,53 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="row">
         <div class="col-md-4">
-
+            <h3>Покупатель</h3>
+            <?= DetailView::widget([
+                'model' => $model->customer,
+                'attributes' => [
+                    'first_name',
+                    'phone_number',
+                ]
+            ]) ?>
         </div>
 
         <div class="col-md-8">
-
+            <h3>Товары в заказе</h3>
+            <div class="row">
+            <?php foreach ($model->products as $product): ?>
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="thumbnail">
+                                <?= Html::img($product->getMainImage()->thumbnail(200, 200, 100, 'order-products')) ?>
+                            </div>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <?= Html::a($product->name, Url::to(['product/update', 'id' => $product->id])) ?>
+                                </div>
+                                <div class="col-md-2">
+                                    <?= $product->price ?>
+                                </div>
+                                <div class="col-md-1">
+                                    x
+                                </div>
+                                <div class="col-md-1">
+                                    <?= $model->getProductQuantity($product) ?>
+                                </div>
+                                <div class="col-md-1">
+                                    =
+                                </div>
+                                <div class="col-md-3">
+                                    <?= $model->getProductAmount($product) ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            </div>
         </div>
     </div>
 
